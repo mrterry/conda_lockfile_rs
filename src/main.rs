e4bdb39b4066f2b775f98e89f4faad1e4ec30b16
Matt Terry <mrterry@nextdoor.com>
2018-10-16 22:30:32 -0700

    another subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate clap;
-extern crate yaml_rust;
+extern crate glob;
 extern crate sha1;
+extern crate yaml_rust;
 
 use std::env;
 use std::error::Error;
@@ -10,11 +11,14 @@ use std::path::PathBuf;
 use std::io::{Error as ioError, ErrorKind as ioErrorKind};
 
 use clap::{App, Arg, SubCommand, ArgMatches};
+use glob::glob;
 use yaml_rust::{YamlLoader};
 
 const SIGIL: &str = "# ENVHASH:";
 
-fn main() -> Result<(), Box<Error>> {
+type Result<T> = std::result::Result<T, Box<Error>>;
+
+fn main() -> Result<()> {
     let app_m = App::new("conda-lockfile")
         .arg(Arg::with_name("v")
              .short("v")
@@ -42,7 +46,10 @@ fn main() -> Result<(), Box<Error>> {
                          .default_value("deps.yml")))
         .subcommand(SubCommand::with_name("checklocks")
                     .arg(Arg::with_name("depfile")
-                         .default_value("deps.yml")))
+                         .default_value("deps.yml"))
+                    .arg(Arg::with_name("depfiles")
+                         .multiple(true))
+                    )
         .get_matches();
 
     match app_m.occurrences_of("v") {
@@ -63,40 +70,54 @@ fn main() -> Result<(), Box<Error>> {
 }
 
 
-fn handle_freeze(matches: &ArgMatches) -> Result<(), Box<Error>> {
+fn handle_freeze(matches: &ArgMatches) -> Result<()> {
     println!("{:?}", matches);
     Ok(())
 }
 
 
-fn handle_create(matches: &ArgMatches) -> Result<(), Box<Error>> {
+fn handle_create(matches: &ArgMatches) -> Result<()> {
     println!("{:?}", matches);
     Ok(())
 }
 
 
-fn get_hash(line: &str) -> Option<&str> {
-    if line.starts_with(SIGIL) {
-        Some(&line[SIGIL.len()..])
-    } else {
-        None
+fn read_sigil_hash<R: Read>(mut f: R) -> Result<String> {
+    let mut file_data = String::new();
+    f.read_to_string(&mut file_data)?;
+    let hash = file_data
+        .lines()
+        .filter_map(|line| if line.starts_with(SIGIL) {Some(&line[10..])} else {None})
+        .map(|line| line.trim())
+        .nth(0);
+    match hash {
+        Some(hash) => Ok(hash.to_string()),
+        None => Err(ioError::new(ioErrorKind::Other, "No Hashes in file").into()),
     }
 }
 
 
-fn handle_checkenv(matches: &ArgMatches) -> Result<(), Box<Error>> {
-    // Get the data from the depfile.
-    let depfile_path = matches.value_of("depfile").unwrap();
-    let mut depfile = File::open(depfile_path)?;
+fn compute_file_hash<R: Read>(mut f: R) -> Result<String> {
     let mut depfile_data = String::new();
-    depfile.read_to_string(&mut depfile_data)?;
+    f.read_to_string(&mut depfile_data)?;
 
     // Hash the contents of the file
     let mut m = sha1::Sha1::new();
     m.update(depfile_data.as_bytes());
-    let expected_hash = m.digest().to_string();
+    Ok(m.digest().to_string())
+}
+
+
+fn handle_checkenv(matches: &ArgMatches) -> Result<()> {
+    // Get the data from the depfile.
+    let depfile_path = matches.value_of("depfile").unwrap();
+    let depfile = File::open(depfile_path)?;
+    let expected_hash = compute_file_hash(depfile)?;
 
     // Extract the name of the environment
+    let mut depfile2 = File::open(depfile_path)?;
+    let mut depfile_data = String::new();
+    depfile2.read_to_string(&mut depfile_data)?;
     let docs = YamlLoader::load_from_str(&depfile_data).unwrap();
     let doc = &docs[0];  // YamlLoader loads multiple documents
     let env_name = doc["name"].as_str().unwrap();
@@ -111,15 +132,8 @@ fn handle_checkenv(matches: &ArgMatches) -> Result<(), Box<Error>> {
     ].iter().collect();
     println!("lockfile_path: {}", lockfile_path.to_str().unwrap());
 
-    let mut lockfile = File::open(lockfile_path)?;
-    let mut lockfile_data = String::new();
-    lockfile.read_to_string(&mut lockfile_data)?;
-    let found_hash = lockfile_data
-        .lines()
-        .filter_map(|line: &str| get_hash(&line))
-        .map(|line| line.trim())
-        .nth(0)
-        .unwrap();
+    let lockfile = File::open(lockfile_path)?;
+    let found_hash = read_sigil_hash(lockfile)?;
 
     if found_hash == expected_hash {
         Ok(())
@@ -131,8 +145,38 @@ fn handle_checkenv(matches: &ArgMatches) -> Result<(), Box<Error>> {
     }
 }
 
+fn find_lockfiles() -> Vec<PathBuf> {
+    let glob_paths: Vec<PathBuf> = glob("deps.yml.*.lock").expect("Failed to read glob pattern")
+        .map(|x| x.unwrap())
+        .collect();
+    glob_paths
+}
 
-fn handle_checklocks(matches: &ArgMatches) -> Result<(), Box<Error>> {
-    println!("{:?}", matches);
-    Ok(())
+fn handle_checklocks(matches: &ArgMatches) -> Result<()> {
+    let depfile_path = matches.value_of("depfile").unwrap();
+    let depfile = File::open(depfile_path)?;
+    let expected_hash = compute_file_hash(depfile)?;
+
+    let lockfiles = match matches.values_of("lockfiles") {
+        Some(files) => files.map(|p| PathBuf::from(p)).collect(),
+        None => find_lockfiles(),
+    };
+
+    let mut success = true;
+    for lockfile_path in lockfiles {
+        let lockfile = File::open(&lockfile_path)?;
+        let found_hash = read_sigil_hash(lockfile)?;
+        if found_hash != expected_hash {
+            success = false;
+            println!("Hashes do not match {:?}, {:?}", depfile_path, lockfile_path);
+            println!("lock    hash: {}", found_hash);
+            println!("depfile hash: {}", expected_hash);
+        }
+    }
+
+    if success {
+        Ok(())
+    } else {
+        Err(ioError::new(ioErrorKind::Other, "Hashes do not match").into())
+    }
 }
