2754b642b41382132907d74a03c78b4d1ad3cc87
Matt Terry <matt.terry@gmail.com>
2018-11-09 09:31:54 -0800

    fix travis (#1)
    * fix travis
    
    * install conda
    
    * run tests
    
    * cleanup
    
    * update version

--- ci/install.sh
+++ ci/install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+if [[ "$(uname)" == "Darwin" ]]; then
+    URL="https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh"
+else
+    URL="https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh"
+fi
+wget $URL -O miniconda.sh;
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 conda config --set always_yes yes --set changeps1 no
@@ -8,4 +13,4 @@ conda update -q conda
 # Useful for debugging any issues with conda
 conda info -a
 
-conda install -q conda-build
+conda install -q conda-build anaconda-client
